8991385f334330e707b45d600cc6cf456049264d
Faith Morante <faith.morante@gmail.com>
2019-02-09 17:54:14 +0800

    Updates

---
+++ precache-manifest.3e16d61b15896f8cbec60d1800addd53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c2883cf7fd2dd42b5f22",
+    "url": "/static/js/main.c2883cf7.chunk.js"
+  },
+  {
+    "revision": "d89d561aff07e4d15e05",
+    "url": "/static/js/1.d89d561a.chunk.js"
+  },
+  {
+    "revision": "c2883cf7fd2dd42b5f22",
+    "url": "/static/css/main.11e04efb.chunk.css"
+  },
+  {
+    "revision": "791c7c29c1e4ffdefb60ba5a8596cf1d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9711d184cd30bea58fd9d4d2bf26dd0e.js"
+  "/precache-manifest.3e16d61b15896f8cbec60d1800addd53.js"
 );
 
 workbox.clientsClaim();
